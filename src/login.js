8693fbe2b39bf24a7fad561614b54ff32d3332d4
allredmatt <matt@allred.digital>
2025-02-27 14:44:58 +0000

    Add JS logic for login

--- src/login.js
+++ src/login.js
@@ -6,27 +6,24 @@ const url = "https://learnifybackend-wvnw.onrender.com"
 async function login(e) {
     e.preventDefault();
 
-    console.log(e.target);
+    const options = {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: e.target.elements[0].value,
+            password: e.target.elements[1].value
+        })
+    };
+    const response = await fetch(`${url}/users/login`, options);
+    const data = await response.json();
 
-    // const options = {
-    //     method: "POST",
-    //     headers: {
-    //         'Accept': 'application/json',
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify({
-    //         username: form.get("username"),
-    //         password: form.get("password")
-    //     })
-    // };
-    //console.log(options);
-    //const response = await fetch(`${url}/users/login`, options);
-    //const data = await response.json();
-
-    // if (response.ok) {
-    //     localStorage.setItem("token", data.token);
-    //     window.location.assign("stats.html");
-    // } else {
-    //     alert(data.error);
-    // }
+    if (response.ok) {
+        localStorage.setItem("token", data.token);
+        window.location.assign("stats.html");
+    } else {
+        alert(data.error);
+    }
 }
