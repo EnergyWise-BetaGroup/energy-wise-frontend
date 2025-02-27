ad963f6356b361a3f6e6cc26116611ab18a3aa50
allredmatt <matt@allred.digital>
2025-02-27 14:35:28 +0000

    Add login logic

--- src/login.js
+++ src/login.js
@@ -1,1 +1,32 @@
 // Login Page
+document.getElementById("login-form").addEventListener("submit", login)
+
+const url = "https://learnifybackend-wvnw.onrender.com"
+
+async function login(e) {
+    e.preventDefault();
+
+    console.log(e.target);
+
+    // const options = {
+    //     method: "POST",
+    //     headers: {
+    //         'Accept': 'application/json',
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //         username: form.get("username"),
+    //         password: form.get("password")
+    //     })
+    // };
+    //console.log(options);
+    //const response = await fetch(`${url}/users/login`, options);
+    //const data = await response.json();
+
+    // if (response.ok) {
+    //     localStorage.setItem("token", data.token);
+    //     window.location.assign("stats.html");
+    // } else {
+    //     alert(data.error);
+    // }
+}
