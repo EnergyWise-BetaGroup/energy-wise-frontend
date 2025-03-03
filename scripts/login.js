d963ac3d364df4a0902e0db10af9efc17265015b
Naeem Shah <naeemultimate@gmail.com>
2025-03-03 12:02:08 +0000

    point queryselector to general form on login page

--- scripts/login.js
+++ scripts/login.js
@@ -1,5 +1,5 @@
 // Login Page
-document.getElementById("login-form").addEventListener("submit", login)
+document.querySelector("form").addEventListener("submit", login);
 
 const url = "http://energywise.ddns.net" //THIS COULD CHANGE
 
