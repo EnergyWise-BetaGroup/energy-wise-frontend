b664a166e1cd9077cc391f88a681b17b425701f3
Naeem Shah <naeemultimate@gmail.com>
2025-03-03 16:52:15 +0000

    try unsuccessfully login test

--- __tests__/helpers.js
+++ __tests__/helpers.js
@@ -1,29 +1,29 @@
-//The Path module provides a way of working with directories and file paths.
-const path = require('path')
+const path = require('path');
 const jsdom = require('jsdom');
-//To use jsdom, you will primarily use the JSDOM constructor, which is a named export of the jsdom main module.
 const { JSDOM } = jsdom;
 
+// Polyfill TextEncoder and TextDecoder
+if (typeof global.TextEncoder === 'undefined') {
+  const { TextEncoder, TextDecoder } = require('util');
+  global.TextEncoder = TextEncoder;
+  global.TextDecoder = TextDecoder;
+}
+
+// Modify `renderDOM` to ignore CSS files
 const renderDOM = async (filename) => {
-  //The process.cwd() method is an inbuilt application programming interface of the process module which is used to get the current working directory of the node.js process.
   const filePath = path.join(process.cwd(), filename);
-
-
-
-
   
-  //Similar to fromURL(), jsdom also provides a fromFile() factory method for constructing a jsdom from a filename
-  // To enable executing scripts inside the page, you can use the runScripts: "dangerously" option
-  //If you want to execute external scripts, included via <script src="">, you'll also need to ensure that they load them. To do this, add the option resources: "usable" as described below. (You'll likely also want to set the url option, for the reasons discussed there.)
   const dom = await JSDOM.fromFile(filePath, {
     runScripts: 'dangerously',
-    resources: 'usable'
+    resources: 'usable',
+    pretendToBeVisual: true,
+    beforeParse(window) {
+      // Prevent JSDOM from throwing errors due to missing stylesheets
+      window.document.querySelectorAll('link[rel="stylesheet"]').forEach((link) => link.remove());
+    }
   });
 
-  return new Promise((resolve, _) => {
-    // When using the JSDOM constructor, you will get back a JSDOM object, which has a number of useful properties, notably window and document to use below
-    // We're basically are saying: 
-    // We wait for the DOM content to have loaded and then we can resolve the promise
+  return new Promise((resolve) => {
     dom.window.document.addEventListener('DOMContentLoaded', () => {
       resolve(dom);
     });
--- __tests__/login.spec.js
+++ __tests__/login.spec.js
@@ -5,86 +5,37 @@ let document;
 
 describe('Login Page - login.html', () => {
   beforeEach(async () => {
-    dom = await renderDOM('login.html'); // Load the login page
+    dom = await renderDOM('login.html'); // Ensure correct path
     document = await dom.window.document;
-  });
 
-  it('has a login form', () => {
-    const form = document.querySelector('form');
-    expect(form).toBeTruthy();
-  });
-
-  it('has a username input field', () => {
-    const usernameInput = document.querySelector('input[type="text"]');
-    expect(usernameInput).toBeTruthy();
-  });
-
-  it('has a password input field', () => {
-    const passwordInput = document.querySelector('input[type="password"]');
-    expect(passwordInput).toBeTruthy();
-  });
-
-  it('has a login button', () => {
-    const loginButton = document.querySelector('button[type="submit"]');
-    expect(loginButton).toBeTruthy();
-    expect(loginButton.innerHTML).toBe('Login');
-  });
-
-  it('prevents form submission with empty fields', async () => {
-    const form = document.querySelector('form');
-    form.dispatchEvent(new dom.window.Event('submit'));
-
-    // Expect no redirect and an alert to show
-    expect(window.location.href).not.toContain('stats.html');
-  });
-  
-
-});
-
-
-/*
-
-it('calls the login function and sends request to API', async () => {
+    // Mock fetch globally for all tests
     global.fetch = jest.fn(() =>
       Promise.resolve({
         ok: true,
-        json: () => Promise.resolve({ token: 'fakeToken123' })
+        json: async () => ({ token: 'mockedToken123' }),
       })
     );
+  });
 
+  afterEach(() => {
+    jest.restoreAllMocks(); // Cleanup mocks after each test
+  });
+
+  it('fetches data successfully and sends API request', async () => {
     const form = document.querySelector('form');
     const usernameInput = document.querySelector('input[type="text"]');
     const passwordInput = document.querySelector('input[type="password"]');
 
-    usernameInput.value = 'testUser';
+    usernameInput.value = 'testuser';
     passwordInput.value = 'password123';
 
+    // Dispatch a submit event
     form.dispatchEvent(new dom.window.Event('submit'));
 
-    expect(fetch).toHaveBeenCalledWith('http://energywise.ddns.net/users/login', expect.any(Object));
-  });
-
-
-  it('displays an error if login fails', async () => {
-    global.fetch = jest.fn(() =>
-      Promise.resolve({
-        ok: false,
-        json: () => Promise.resolve({ error: 'Invalid credentials' })
-      })
+    // Expect fetch to have been called with the correct API URL
+    expect(fetch).toHaveBeenCalledWith(
+      'http://energywise.ddns.net/users/login',
+      expect.any(Object)
     );
-
-    window.alert = jest.fn(); // Mock alert function
-
-    const form = document.querySelector('form');
-    const usernameInput = document.querySelector('input[type="text"]');
-    const passwordInput = document.querySelector('input[type="password"]');
-
-    usernameInput.value = 'wrongUser';
-    passwordInput.value = 'wrongPassword';
-
-    form.dispatchEvent(new dom.window.Event('submit'));
-
-    expect(window.alert).toHaveBeenCalledWith('Invalid credentials');
   });
-
-*/
+});
