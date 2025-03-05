10be0fd9912589ea8550b03516294b3de2ff5d09
allredmatt <matt@allred.digital>
2025-03-05 11:48:12 +0000

    Add gauge chart fetch code

--- scripts/stats.js
+++ scripts/stats.js
@@ -2,17 +2,14 @@ const url = "http://energywise.ddns.net:3000"
 
 const lineMountPoint = document.getElementById("line-mount-point")
 const donutMountPoint = document.getElementById("donut-mount-point")
+const gauge1MountPoint = document.getElementById("gauge1-mount-point")
+const gauge2MountPoint = document.getElementById("gauge2-mount-point")
+const currentMountPoint = document.getElementById("current-mount-point")
+
 
 const token = localStorage.getItem("token");
 
-async function fetchChart(chart ,mountPoint){
-    options = {
-        headers: {authorization: token}
-    }
-
-    const response = await fetch(`${url}/stats/${chart}`, options)
-    const returnedData = await response.json()
-
+function mountHtml (mountPoint, returnedData) {
     if(returnedData.html) {
         mountPoint.innerHTML = returnedData.html
     } else {
@@ -25,9 +22,39 @@ async function fetchChart(chart ,mountPoint){
     }
 }
 
+async function fetchChart(chart ,mountPoint){
+    options = {
+        headers: {authorization: token}
+    }
+
+    const response = await fetch(`${url}/stats/${chart}`, options)
+    const returnedData = await response.json()
+
+    mountHtml(mountPoint, returnedData)
+}
+
+async function fetchGauge (gauge1, gauge2, current) {
+    options = {
+        headers: {authorization: token}
+    }
+
+    const response = await fetch(`${url}/stats/gauge`, options)
+    const returnedData = await response.json()
+
+    gauge1Chart = returnedData.gauge1
+    gauge2Chart = returnedData.gauge2
+    currentData = returnedData.current 
+
+    mountHtml(gauge1, gauge1Chart)
+    mountHtml(gauge2, gauge2Chart)
+    
+    current.innerHTML = currentData
+}
+
 function fetchAllCharts() {
     fetchChart("line-graph", lineMountPoint)
     fetchChart("donut", donutMountPoint)
+    fetchGauge(gauge1MountPoint, gauge2MountPoint, currentMountPoint)
 }
 
 window.addEventListener("DOMContentLoaded", fetchAllCharts)
